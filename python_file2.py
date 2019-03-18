dc10df2d2721c4ecc3f1f9c8e93abc49e8ed97c3
Gano97 <s235972@gmail.com>
2019-03-18 15:14:07 +0100

    Es 2 Lab 2

---
+++ python_file2.py
@@ -0,0 +1,36 @@
+scelta=0
+n_task=0
+task=[]
+from sys import argv
+fp= argv[1]
+txt= open(fp)
+for strng in txt.read().splitlines():
+    task.append(strng)
+    n_task+=1
+while scelta!=4:
+    print("Task Manager")
+    print("1. Insert a new task (a string of text)")
+    print("2. Remove a task (by typing its content, exactly)")
+    print("3. Show all existing tasks, sorted in alphabetic order")
+    print("4. Close the program")
+    scelta=int(input("Make your choice: "))
+    if(scelta==1):
+        task.append(input("Insert task's content: "))
+        n_task+=1
+    elif(scelta==2):
+        if(n_task<=0):
+            print("No tasks")
+        else:
+            task.remove(input("Insert task's content: "))
+    elif(scelta==3):
+        if (n_task <= 0):
+            print("No tasks")
+        else:
+            print(sorted(task))
+    elif(scelta==4):
+        print("The End")
+        txt.close()
+        txt= open(fp, "w")
+        for strng in task:
+            txt.write(strng+"\n")
+        txt.close()
